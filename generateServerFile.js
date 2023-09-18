0897d40e020ffcedeff9dda58c919c2600522f61
zer0cheros <christian.wiss@optimaedu.fi>
2023-09-18 08:33:56 +0300

    a

--- generateServerFile.js
+++ generateServerFile.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const reactSrcPath = path.join(__dirname, './src'); 
+const reactSrcPath = path.join(__dirname, './src');
+const serverFilePath = path.join(reactSrcPath, 'server.ts');
+
+// Check if the 'src' folder exists, and create it if it doesn't.
+if (!fs.existsSync(reactSrcPath)) {
+  fs.mkdirSync(reactSrcPath);
+}
 
 const serverCode = `
 import Server from 'react-simple-server'
 
-const server = new Server({port:3000})
+const server = new Server({port: 3000})
 server.connect('db', {client: "mysql", host: '127.0.0.1', port: 3306, user: 'root', password: 'password'})
 server.Get('/')
 server.Get('/users', true).DB('users')
@@ -15,5 +21,5 @@ server.Get('/user', true).DB('users')
 server.start()
 `;
 
-fs.writeFileSync(path.join(reactSrcPath, 'server.ts'), serverCode);
-console.log('server.ts file created in React src folder');
+fs.writeFileSync(serverFilePath, serverCode);
+console.log(`server.ts file created in the 'src' folder.`);
